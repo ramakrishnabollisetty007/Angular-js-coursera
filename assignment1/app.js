ff0cb2fe1f8983e993a2048714d1ed4c8b77fd71
Ram <ramakrishnabolisetty007@gmail.com>
2020-05-26 20:45:10 +0530

    Version 1 of assignment

---
+++ assignment1/app.js
@@ -0,0 +1,48 @@
+(function () {
+  'use strict';
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject =['$scope'];
+  function LunchCheckController($scope) {
+    $scope.items="";
+
+    $scope.splitChecker=function () {
+    if(!$scope.items)
+    {
+      $scope.msg="Please enter data first";
+      $scope.getStyle($scope.msg);
+
+    }else{
+      var itemValues=$scope.items;
+      var subString=itemValues.split(',');
+
+      if(subString.length > 3)
+      {
+        $scope.msg="Too Much";
+        $scope.getStyle($scope.msg);
+      }
+      else {
+        $scope.msg="enjoy!";
+$scope.getStyle($scope.msg);
+      }
+
+    }
+    };
+
+
+    $scope.getStyle=function (msg) {
+      if(msg=="enjoy!"){
+      return {'color':'green'};
+    }else if(msg=="Too Much")
+    {
+        return {'color':'green'};
+      }else {
+        return {'color':'red'};
+      }
+    };
+
+  };
+
+
+
+})();
